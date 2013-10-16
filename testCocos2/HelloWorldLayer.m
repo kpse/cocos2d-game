1eeac376a54e54c4418b6e9f078ec8ea179d6c16
Suo Qin <kpsesuo@gmail.com>
2013-10-16 13:27:22 +0800

    init

--- testCocos2/HelloWorldLayer.m
+++ testCocos2/HelloWorldLayer.m
@@ -12,11 +12,15 @@
 
 // Needed to obtain the Navigation Controller
 #import "AppDelegate.h"
+#import "SimpleAudioEngine.h"
 
 #pragma mark - HelloWorldLayer
 
 // HelloWorldLayer implementation
-@implementation HelloWorldLayer
+@implementation HelloWorldLayer{
+    NSMutableArray * _monsters;
+    NSMutableArray * _projectiles;
+}
 
 // Helper class method that creates a Scene with the HelloWorldLayer as the only child.
 +(CCScene *) scene
@@ -39,83 +43,25 @@
 {
 	// always call "super" init
 	// Apple recommends to re-assign "self" with the "super's" return value
-	if( (self=[super init]) ) {
-		
-		// create and initialize a Label
-		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello World" fontName:@"Marker Felt" fontSize:64];
+    if ((self = [super initWithColor:ccc4(255, 255, 255, 255)])) {
+        CGSize winSize = [CCDirector sharedDirector].winSize;
+        CCSprite *player = [CCSprite spriteWithFile:@"player.png"];
+        player.position = ccp(player.contentSize.width/2, winSize.height/2);
+        [self addChild:player];
 
-		// ask director for the window size
-		CGSize size = [[CCDirector sharedDirector] winSize];
-	
-		// position the label on the center of the screen
-		label.position =  ccp( size.width /2 , size.height/2 );
-		
-		// add the label as a child to this Layer
-		[self addChild: label];
-		
-		
-		
-		//
-		// Leaderboards and Achievements
-		//
-		
-		// Default font size will be 28 points.
-		[CCMenuItemFont setFontSize:28];
-		
-		// to avoid a retain-cycle with the menuitem and blocks
-		__block id copy_self = self;
-		
-		// Achievement Menu Item using blocks
-		CCMenuItem *itemAchievement = [CCMenuItemFont itemWithString:@"Achievements" block:^(id sender) {
-			
-			
-			GKAchievementViewController *achivementViewController = [[GKAchievementViewController alloc] init];
-			achivementViewController.achievementDelegate = copy_self;
-			
-			AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
-			
-			[[app navController] presentModalViewController:achivementViewController animated:YES];
-			
-			[achivementViewController release];
-		}];
-		
-		// Leaderboard Menu Item using blocks
-		CCMenuItem *itemLeaderboard = [CCMenuItemFont itemWithString:@"Leaderboard" block:^(id sender) {
-			
-			
-			GKLeaderboardViewController *leaderboardViewController = [[GKLeaderboardViewController alloc] init];
-			leaderboardViewController.leaderboardDelegate = copy_self;
-			
-			AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
-			
-			[[app navController] presentModalViewController:leaderboardViewController animated:YES];
-			
-			[leaderboardViewController release];
-		}];
+        [self schedule:@selector(gameLogic:) interval:1.0];
 
-		
-		CCMenu *menu = [CCMenu menuWithItems:itemAchievement, itemLeaderboard, nil];
-		
-		[menu alignItemsHorizontallyWithPadding:20];
-		[menu setPosition:ccp( size.width/2, size.height/2 - 50)];
-		
-		// Add the menu to the layer
-		[self addChild:menu];
+        [self setIsTouchEnabled:YES];
 
-	}
-	return self;
+        _monsters = [[NSMutableArray alloc] init];
+        _projectiles = [[NSMutableArray alloc] init];
+        [self schedule:@selector(update:)];
+
+        [[SimpleAudioEngine sharedEngine] playBackgroundMusic:@"background-music-aac.caf"];
+    }
+    return self;
 }
 
-// on "dealloc" you need to release all your retained objects
-- (void) dealloc
-{
-	// in case you have something to dealloc, do it in this method
-	// in this particular example nothing needs to be released.
-	// cocos2d will automatically release all the children (Label)
-	
-	// don't forget to call "super dealloc"
-	[super dealloc];
-}
 
 #pragma mark GameKit delegate
 
@@ -130,4 +76,119 @@
 	AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
 	[[app navController] dismissModalViewControllerAnimated:YES];
 }
+
+-(void)gameLogic:(ccTime)dt {
+    [self addMonster];
+}
+
+- (void) addMonster {
+
+    CCSprite * monster = [CCSprite spriteWithFile:@"monster.png"];
+
+    // Determine where to spawn the monster along the Y axis
+    CGSize winSize = [CCDirector sharedDirector].winSize;
+    int minY = monster.contentSize.height / 2;
+    int maxY = winSize.height - monster.contentSize.height/2;
+    int rangeY = maxY - minY;
+    int actualY = (arc4random() % rangeY) + minY;
+
+    // Create the monster slightly off-screen along the right edge,
+    // and along a random position along the Y axis as calculated above
+    monster.position = ccp(winSize.width + monster.contentSize.width/2, actualY);
+    [self addChild:monster];
+
+    // Determine speed of the monster
+    int minDuration = 2.0;
+    int maxDuration = 4.0;
+    int rangeDuration = maxDuration - minDuration;
+    int actualDuration = (arc4random() % rangeDuration) + minDuration;
+
+    // Create the actions
+    CCMoveTo * actionMove = [CCMoveTo actionWithDuration:actualDuration
+                                                position:ccp(-monster.contentSize.width/2, actualY)];
+    CCCallBlockN * actionMoveDone = [CCCallBlockN actionWithBlock:^(CCNode *node) {
+        [node removeFromParentAndCleanup:YES];
+        [_monsters removeObject:node];
+    }];
+    [monster runAction:[CCSequence actions:actionMove, actionMoveDone, nil]];
+    monster.tag = 1;
+    [_monsters addObject:monster];
+}
+
+- (void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+
+    // Choose one of the touches to work with
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [self convertTouchToNodeSpace:touch];
+
+    // Set up initial location of projectile
+    CGSize winSize = [[CCDirector sharedDirector] winSize];
+    CCSprite *projectile = [CCSprite spriteWithFile:@"projectile.png"];
+    projectile.position = ccp(20, winSize.height/2);
+
+    // Determine offset of location to projectile
+    CGPoint offset = ccpSub(location, projectile.position);
+
+    // Bail out if you are shooting down or backwards
+    if (offset.x <= 0) return;
+
+    // Ok to add now - we've double checked position
+    [self addChild:projectile];
+
+    int realX = winSize.width + (projectile.contentSize.width/2);
+    float ratio = (float) offset.y / (float) offset.x;
+    int realY = (realX * ratio) + projectile.position.y;
+    CGPoint realDest = ccp(realX, realY);
+
+    // Determine the length of how far you're shooting
+    int offRealX = realX - projectile.position.x;
+    int offRealY = realY - projectile.position.y;
+    float length = sqrtf((offRealX*offRealX)+(offRealY*offRealY));
+    float velocity = 480/1; // 480pixels/1sec
+    float realMoveDuration = length/velocity;
+
+    // Move projectile to actual endpoint
+    [projectile runAction:
+        [CCSequence actions:
+            [CCMoveTo actionWithDuration:realMoveDuration position:realDest],
+            [CCCallBlockN actionWithBlock:^(CCNode *node) {
+                [node removeFromParentAndCleanup:YES];
+                [_projectiles removeObject:node];
+            }],
+            nil]];
+    projectile.tag = 2;
+    [_projectiles addObject:projectile];
+    [[SimpleAudioEngine sharedEngine] playEffect:@"pew-pew-lei.caf"];
+}
+
+- (void)update:(ccTime)dt {
+
+    NSMutableArray *projectilesToDelete = [[NSMutableArray alloc] init];
+    for (CCSprite *projectile in _projectiles) {
+
+        NSMutableArray *monstersToDelete = [[NSMutableArray alloc] init];
+        for (CCSprite *monster in _monsters) {
+
+            if (CGRectIntersectsRect(projectile.boundingBox, monster.boundingBox)) {
+                [monstersToDelete addObject:monster];
+            }
+        }
+
+        for (CCSprite *monster in monstersToDelete) {
+            [_monsters removeObject:monster];
+            [self removeChild:monster cleanup:YES];
+        }
+
+        if (monstersToDelete.count > 0) {
+            [projectilesToDelete addObject:projectile];
+        }
+
+    }
+
+    for (CCSprite *projectile in projectilesToDelete) {
+        [_projectiles removeObject:projectile];
+        [self removeChild:projectile cleanup:YES];
+    }
+}
+
 @end
